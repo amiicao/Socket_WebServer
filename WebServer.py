81941cc5a3ccf45c4d7a6c4cb34df1a127205ea9
Amy Cao <Amy.369@hotmail.com>
2022-12-04 18:33:24 -0800

    enable caching system to support status codes 304 and 404

--- WebServer.py
+++ WebServer.py
@@ -1,5 +1,71 @@
 # Include Python's Socket Library
+import shutil
 from socket import *
+
+response_status_text = {
+    '200': 'OK',
+    '304': 'Not Modified',
+    '400': 'Bad request',
+    '404': 'Not Found',
+    '408': 'Request Timed Out'
+}
+
+
+def fetch_file(filename):
+    print("client requested filename", filename)
+
+    try:
+        cache_f = open(f'cache_files/{filename}', 'r')
+        cache_file_lines = cache_f.readlines()
+        cache_f.close()
+
+        server_f = open(f'server_files/{filename}', 'r')
+        server_file_lines = server_f.readlines()
+        server_f.close()
+
+        # compare cache file with server file; if different, status code 304; else status code 200
+        if cache_file_lines != server_file_lines:
+            print("cached file is different from server file!")
+            status_code = '404'  # todo: hack to enable something to show on webpage; 304 doesn't work
+            response_html = ['<html><body><h1>Error 304</h1>', '<p>Not Modified</p>', '</body></html>']
+            response_body = ''.join(response_html)
+            return response_body, status_code
+
+        else:
+            print("cached file is same as server file!")
+            status_code = '200'
+            response_body = ''.join(cache_file_lines)
+            return response_body, status_code
+
+    except:  # file is not in cache, need to fetch it from server
+        if fetch_file_from_server(filename):
+            cache_f = open(f'cache_files/{filename}', 'r')
+            cache_file_lines = cache_f.readlines()
+            cache_f.close()
+
+            status_code = '200'
+            response_body = ''.join(cache_file_lines)
+            return response_body, status_code
+
+        else:
+            print(f'server does not have {filename}!!')
+            status_code = '404'
+            response_html = ['<html><body><h1>Error 404</h1>', '<p>Not Found</p>', '</body></html>']
+            response_body = ''.join(response_html)
+            return response_body, status_code
+
+
+def fetch_file_from_server(filename):
+    print(f"trying to fetch {filename} from server")
+
+    try:
+        server_f = open(f'server_files/{filename}', 'r')
+        server_f.close()
+        shutil.copy2(f'server_files/{filename}', f'cache_files/{filename}')
+        return True
+
+    except:
+        return False
 
 
 def normalize_line_endings(s):
@@ -58,20 +124,17 @@ def run():
 
         # Read from socket (but not address as in UDP)
         data = connectionSocket.recv(1024).decode()
-        time_out_flag = False
 
-        # -- STATUS CODE 404 -- check if timeout
-        # data, time_out_flag = receive_chunks(connectionSocket) # todo: add 404 feature function
-        print("data", data)
-        print("timeoutflag", time_out_flag)
+        # data, response_status = receive_chunks(connectionSocket) # todo: add 404 feature function
+        print("\n----data", data, "\n------------------------------\n")
 
         request = normalize_line_endings(data)
         request_head, request_body = request.split('\n\n', 1)
 
-        print("\n----request_head:", request_head)
-        print("\n----request_body", request_body)
+        print("\n----request_head:", request_head, "\n------------------------------\n")
+        print("\n----request_body", request_body, "\n------------------------------\n")
 
-        # first line is request headline, and others are headers
+        # first line is request headline, and others are headers # todo: clean this part up
         request_head = request_head.splitlines()
         request_headline = request_head[0]
 
@@ -81,49 +144,30 @@ def run():
         request_method, request_uri, request_proto = request_headline.split(' ', 3)
 
         print("filename", request_uri)
-        # -- STATUS CODE 404 -- check if the page requested exists
-        file_not_found = False
-        try:
-            f = open(request_uri[1:], 'r')
-            file_lines = f.readlines()
-            response_body = ''.join(file_lines)
 
-        except:
-            file_not_found = True
-            response_html = ['<html><body><h1>Error 404</h1>', '<p>Not Found</p>', '</body></html>']
-            response_body = ''.join(response_html)
+        response_body, response_status = fetch_file(request_uri[1:])
 
+        # build response status
+        response_status_line = f'HTTP/1.1 {response_status} {response_status_text[response_status]}'
+
+        # build response headers
         response_headers = {
             'Content-Type': 'text/html; encoding=utf8',
             'Content-Length': len(response_body),
             'Connection': 'close',
         }
-
         response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in \
                                        iter(response_headers.items()))
 
-        # build response status
-        response_proto = 'HTTP/1.1'
-
-        if time_out_flag:
-            response_status = '408'
-            response_status_text = 'Request Timed Out'
-        elif file_not_found:
-            response_status = '404'
-            response_status_text = 'Not Found'
-        else:
-            response_status = '200'
-            response_status_text = 'OK'  # this can be random
-
-        response_status = f'{response_proto} {response_status}  {response_status_text}'
-        # sending all this stuff
-        connectionSocket.send(response_status.encode())
+        # sending full response
+        connectionSocket.send(response_status_line.encode())
         connectionSocket.send(response_headers_raw.encode())
         connectionSocket.send('\n'.encode())  # to separate headers from body
-
         connectionSocket.send(response_body.encode())
 
+        print(response_body)
         # Close connection to client (but not welcoming socket)
         connectionSocket.close()
+
 
 run()
